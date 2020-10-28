25a0110aeecc81ef9ce5c15ce7a41bcf46118be1
Renan Palomino Granusso <renan.granusso@gmail.com>
2020-10-28 11:29:09 -0300

    Commit 11

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,58 @@ from app import app
 
 from flask import render_template
 
-@app.route('/') #raiz do sistema.
+from flask import request
 
-def home():
+from flask_sqlalchemy import SQLAlchemy
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1234@localhost/bd_trabalho'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Doenca(db.Model):
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    nome = db.Column(db.String(100))
+    sintomas = db.Column(db.String(100))
+
+    def __init__(self, nome, sintomas):
+        self.nome = nome
+        self.sintomas = sintomas
+
+class Epidemiologico(db.Model):
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    data_coleta = db.Column(db.String(100))
+    doenca_associada = db.Column(db.String(100))
+
+    def __init__(self, data_coleta, doenca_associada):
+        self.data_coleta = data_coleta
+        self.doenca_associada = doenca_associada
+
+db.create_all()
+
+@app.route('/') #raiz do sistema.
+def index():
     return render_template('index.html')
+
+
+@app.route('/cadastro_doenca', methods=['GET', 'POST'])
+def cadastro_doenca():
+    return render_template('cadastro_doenca.html')
+
+@app.route('/cadastro_epidemiologico', methods=['GET' , 'POST'])
+def cadastro_epidemiologico():
+    if request.method == 'POST':
+        epidemio = Epidemiologico(request.form['data_coleta'], request.form['doenca_associada'])
+        db.session.add(epidemio)
+        db.session.commit()
+        #return redirect(url_for('index'))
+    return render_template('cadastro_epidemiologico.html')
+
+@app.route('/visualizacao_doencas', methods=['GET'])
+def visualizacao_doencas():
+    return render_template('visualizacao_doencas.html')
+
+@app.route('/visualizacao_epidemiologica', methods=['GET'])
+def visualizacao_epidemiologica():
+    Epidemio = Epidemiologico.query.all()
+    return render_template('visualizacao_epidemiologica.html', Epidemio=Epidemio)
--- controle.py
+++ controle.py
@@ -1,4 +1,4 @@
 from app import app
 
 if __name__ == "__main__": #verificar o contexto que está sendo executado o modulo de controle.
-    app.run(debug=True, port=8080)
+    app.run(debug=True, port=80)
