c0f8cd5bd852a9b8a57da43f4df9e5905ab829cd
Renan Palomino Granusso <renan.granusso@gmail.com>
2020-11-18 11:06:31 -0300

    atualização js

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,2 +1,22 @@
-let d = new Date();
-document.getElementById("mostrarData").innerHTML = d;
+var hora = document.getElementById('mostrarHora');
+
+function time() {
+  var d = new Date();
+  var s = d.getSeconds();
+  var m = d.getMinutes();
+  var h = d.getHours();
+
+  if (h < 10){
+      h = "0" + h
+  }
+  if (m < 10){
+      m = "0" + m
+  }
+  if(s < 10){
+      s = "0" + s
+  }
+  
+  hora.textContent = "Hora Atual: " + h + ":" + m + ":" + s
+}
+
+setInterval(time, 1000);
